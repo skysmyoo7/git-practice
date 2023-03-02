e7b8ccf86afa107549b7402e5e037b4a5b66ac6d
skysmyoo7 <skysmyoo7@gmail.com>
2023-03-02 09:21:11 +0900

    test 파일 안에 있는 내용 전부 삭제

--- src/test.kt
+++ src/test.kt
@@ -1,15 +1,3 @@
 fun main() {
 
-    val test = "테스트용"
-    val c = "사라져라 뿅"
-    val d = "d"
-    val e = "e"
-
-    val test = "test"
-    var test2 = "test2"
-    var test3 = "test3"
-
-    val text = "변경사항"
-
-    val text3 = "여기는 test4"
 }
